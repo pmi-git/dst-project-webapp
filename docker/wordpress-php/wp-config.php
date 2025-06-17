969c0582dca0119518394ec905d457b8e824bd95
pmi-git <patrick.miviere@gmail.com>
2025-06-17 22:06:37 +0200

    unset debug

--- docker/wordpress-php/wp-config.php
+++ docker/wordpress-php/wp-config.php
@@ -4,7 +4,8 @@ define( 'DB_USER', getenv('WORDPRESS_DB_USER') ?: 'wpuser' );
 define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') ?: 'wppass' );
 define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') ?: 'mysql' );
 
-define( 'WP_DEBUG', getenv('WP_DEBUG') ?: false );
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
 define( 'WP_CACHE', getenv('WP_CACHE') ?: false );
 
 define( 'FS_METHOD', 'direct' );
