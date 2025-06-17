1d186fc3a2e71f79fea44422257239698e0be122
pmi-git <patrick.miviere@gmail.com>
2025-06-17 21:23:40 +0200

    modif path

--- compose/rebuild.sh
+++ compose/rebuild.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Arrêt des conteneurs et suppression des volumes..."
-docker-compose -f compose/docker-compose.base.yml down -v
+docker-compose -f docker-compose.base.yml down -v
 
 echo "Reconstruction complète des images..."
-docker-compose -f compose/docker-compose.base.yml build --no-cache
+docker-compose -f docker-compose.base.yml build --no-cache
 
 echo "Lancement des conteneurs en arrière-plan..."
-docker-compose -f compose/docker-compose.base.yml up -d
+docker-compose -f docker-compose.base.yml up -d
 
 echo "Environnement relancé avec succès."
