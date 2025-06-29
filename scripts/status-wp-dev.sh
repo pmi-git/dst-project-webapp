2f8461e2996642c811640f3bf86803da138f5859
pmi-git <patrick.miviere@gmail.com>
2025-06-29 15:23:37 +0200

    maria vs mysql dans le grep

--- scripts/status-wp-dev.sh
+++ scripts/status-wp-dev.sh
@@ -10,4 +10,4 @@ docker-compose \
 
 echo ""
 echo "Vérification des statuts de santé :"
-docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}" | grep -E "wp-nginx|wp-php|wp-mysql|wp-redis"
+docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}" | grep -E "wp-nginx|wp-php|wp-mariadb|wp-redis"
